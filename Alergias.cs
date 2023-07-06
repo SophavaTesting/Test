54b3c3a4a456870748c02ed1e661d182a04a2c4c
SophavaTesting <138824956+SophavaTesting@users.noreply.github.com>
2023-07-06 18:49:41 -0500

    Create class Alergias

---
+++ Alergias.cs
@@ -0,0 +1,20 @@
+// Adicionar librerias si lo requiere
+
+// No modifique la siguiente enumeracion
+[Flags]
+public enum Alergeno
+{
+  Huevos = 1,
+  Cacahuetes = 2,
+  Mariscos = 4,
+  Fresas = 8,
+  Tomates = 16,
+  Chocolate = 32,
+  Polen = 64,
+  Gatos = 128
+}
+  
+public class Alergias
+{
+  // Constructores, Propiedades y Metodos
+}
